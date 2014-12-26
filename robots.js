49a9f76266198df23fda6cabd7915dce91d5fc40
Jasmina Kruhak <jkruhak@gmail.com>
2014-12-26 17:03:22 +0400

    end of ch. 5

---
+++ robots.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+	$("#background1, #bgContainer, .one").on("click", function() {
+		$(".robotUnit, #bikerack, #background1").addClass("move");
+	});
+
+	var transEndEventNames = {
+		'WebkitTransition' : 'WebkitTransitionEnd',
+		'MozTransition' : 'transitionend',
+		// per Ian Lunn at http://www.ianlunn.co.uk/blog/articles/opera-12-otransitionend-bugs-and-workarounds/
+		// in Opera 12 they changed the case of oTransitionEnd
+		// to be otransitionend instead
+		'OTransition' : 'oTransitionEnd otransitionend',
+		'msTransition' : 'MSTransitionEnd',
+		'transition' : 'transitionend'
+	},
+
+	transEndEventName = transEndEventNames[ Modernizr.prefixed('transition') ];
+
+	$('.robotUnit').on(transEndEventName, function() {
+		$('.one').addClass("invisible");
+		$('.two').addClass("visible");
+	});
+
+	$('.two').on('click', function() {
+		console.log('clicked');
+		$('#background1').addClass('slideOut');
+		$('#background2').addClass('slideIn');
+		$('#cat').addClass('visible');
+	});
+
+	$('#background2').on(transEndEventName, function() {
+		$('.two').removeClass("visible");
+		$('.three').addClass("visible");
+	});
+
+	$('.three').on('click', function() {
+		$('#robot2, #cat, #background2, #inside').addClass('move');
+	});
+});
