822f46bd798b2d934ea331e5f6923ac560a5fbe0
Jasmina Kruhak <jkruhak@gmail.com>
2015-01-17 19:35:15 +0400

    ch. 11 mid

--- drinks.js
+++ drinks.js
@@ -6,3 +6,16 @@ function setup() {
 		revealRecipe($(this));
 	});
 }
+
+function revealRecipe($elem) {
+	var drinkIndex = $elem.closest('section').index('#bar section') + 1;
+
+	$('.recipes ul:nth-of-type(' + drinkIndex + ')').addClass('visible');
+	$('.recipes h3:nth-of-type(' + drinkIndex + ')').addClass('visible');
+}
+
+function resetClasses($elem) {
+	$('.recipes ul, .recipes h3, .recipes ul li').removeClass();
+	$('.recipes').removeClass('row2');
+	$('.recipes').addClass('visible');
+}
