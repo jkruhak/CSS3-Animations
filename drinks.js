9a3c38a3cce7eeb76b5f3f30d17a5f558914724d
Jasmina Kruhak <jkruhak@gmail.com>
2015-01-18 06:41:08 +0400

    ch.11

--- drinks.js
+++ drinks.js
@@ -5,6 +5,20 @@ function setup() {
 		resetClasses($(this));
 		revealRecipe($(this));
 	});
+
+	$('.tumbler div').on('click', function() {
+		resetClasses($(this));
+		revealRecipe($(this));
+
+		//add one since nth-of-type(1) is the first elem; nth-of-type(0) is null
+		var ingredientIndex = $(this).index() + 1;
+
+		//find the right ingredient element
+		var elem = '.recipes ' + 'li:nth-of-type(' + ingredientIndex + ')';
+
+		//add highlight class to the right ingredient in the paper
+		$(elem).addClass('hightlight');
+	});
 }
 
 function revealRecipe($elem) {
@@ -15,7 +29,15 @@ function revealRecipe($elem) {
 }
 
 function resetClasses($elem) {
+	//removes ALL classes from the elems that match these jQuery selectors
 	$('.recipes ul, .recipes h3, .recipes ul li').removeClass();
+
 	$('.recipes').removeClass('row2');
 	$('.recipes').addClass('visible');
+
+	var drinkIndex = $elem.closest('section').index('#bar section') + 1;
+
+	if(drinkIndex >= 4) {
+		$('.recipes').addClass('row2');
+	}
 }
